71403e12b80693ebc20df46723ab7889b7175c8e
John Reah <john.reah@johnreah.com>
2019-11-07 17:23:44 +0000

    Start tomcat from systemd

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -19,12 +19,12 @@ yum install -y htop
 
 # install httpd
 yum install -y httpd.x86_64
-systemctl start httpd.service
 systemctl enable httpd.service
+systemctl start httpd.service
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
 
 # install open-jdk
-yum install -y java-1.8.0-openjdk
+#yum install -y java-1.8.0-openjdk
 
 # use amazon extras to instalk JDK11
 # test with alternatives --config java
@@ -40,30 +40,57 @@ amazon-linux-extras install java-openjdk11 -y
 # webapps:  /usr/share/tomcat/webapps  -> /var/lib/tomcat/webapps
 # work:     /usr/share/tomcat/work     -> /var/cache/tomcat/work
 # service:  /usr/lib/systemd/system/tomcat.service
-amazon-linux-extras install tomcat8.5 -y
-chown -R tomcat:tomcat /usr/share/tomcat
-systemctl enable tomcat.service
-systemctl start tomcat.service
-yum install -y tomcat-webapps
-yum install -y tomcat-admin-webapps
+#amazon-linux-extras install tomcat8.5 -y
+#chown -R tomcat:tomcat /usr/share/tomcat
+#systemctl enable tomcat.service
+#systemctl start tomcat.service
+#yum install -y tomcat-webapps
+#yum install -y tomcat-admin-webapps
 
-# Install Tomcat 9 (not systemd)
-useradd -m -U -d /opt/tomcat -s /bin/false tomcat
+# Install Tomcat 9 (not available in amazon-linux-extras)
+useradd --no-create-home -U -d /opt/tomcat -s /bin/false tomcat
 cd /tmp
 wget http://apache.mirror.anlx.net/tomcat/tomcat-9/v9.0.27/bin/apache-tomcat-9.0.27.tar.gz
 tar -xf apache-tomcat-9.0.27.tar.gz
-mv apache-tomcat-9.0.27 /opt/tomcat
-chown -R tomcat:tomcat /opt/tomcat
-cat > /etc/systemd/system/tomcat2.service <<- EOF
-	line1
-	line2
+mv apache-tomcat-9.0.27 /opt
+ln -s /opt/apache-tomcat-9.0.27 /opt/tomcat
+chown -R -H tomcat:tomcat /opt/tomcat
+cat > /etc/systemd/system/tomcat.service <<- EOF
+	[Unit]
+	Description=Apache Tomcat Web Application Container
+	Wants=network.target
+	After=network.target
+
+	[Service]
+	Type=forking
+
+	Environment=JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.2.7-0.amzn2.x86_64
+	Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+	Environment=CATALINA_HOME=/opt/tomcat
+	Environment='CATALINA_OPTS=-Xms512M -Xmx1G -Djava.net.preferIPv4Stack=true'
+	Environment='JAVA_OPTS=-Djava.awt.headless=true'
+
+	ExecStart=/opt/tomcat/bin/startup.sh
+	ExecStop=/opt/tomcat/bin/shutdown.sh
+	SuccessExitStatus=143
+
+	User=tomcat
+	Group=tomcat
+	UMask=0007
+	RestartSec=10
+	Restart=always
+
+	[Install]
+	WantedBy=multi-user.target
 EOF
+systemctl enable tomcat
+systemctl start tomcat
 
 # MariaDB
 yum install -y mariadb-server
 systemctl enable mariadb.service
 systemctl start mariadb.service
 
-# Environment variables
+# Environment variables - don't put real password in git
 echo "export JOKES_DB_USER=jokes_grails" > /etc/profile.d/variables.sh
 echo "export JOKES_DB_PASSWORD=jokes_grails" >> /etc/profile.d/variables.sh
